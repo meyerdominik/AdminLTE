3a01d1e7a062853de5451f4e5c1422225d3316b5
Dominik <mail@meyerdominik.de>
2020-03-29 18:33:28 +0200

    Header status

--- scripts/pi-hole/php/header.php
+++ scripts/pi-hole/php/header.php
@@ -260,80 +260,74 @@ if($auth) {
         </nav>
 		
 		<?php
+		
+		$DominikPIStatus = "";
+		$DominikPITemp = "";
+		$DominikPIMem = "";
+		
 		$pistatus = exec('sudo pihole status web');
 		if ($pistatus == "1") {
-			echo '<a id="status"><i class="fa fa-circle" style="color:#7FFF00"></i> Active</a>';
+			$DominikPIStatus = '<i class="fa fa-circle" style="color:#7FFF00"/> Active';
 		} elseif ($pistatus == "0") {
-			echo '<a id="status"><i class="fa fa-circle" style="color:#FF0000"></i> Offline</a>';
+			$DominikPIStatus = '<i class="fa fa-circle" style="color:#FF0000"/> Offline';
 		} elseif ($pistatus == "-1") {
-			echo '<a id="status"><i class="fa fa-circle" style="color:#FF0000"></i> DNS service not running</a>';
+			$DominikPIStatus = '<i class="fa fa-circle" style="color:#FF0000"/> DNS service not running';
 		} else {
-			echo '<a id="status"><i class="fa fa-circle" style="color:#ff9900"></i> Unknown</a>';
+			$DominikPIStatus = '<i class="fa fa-circle" style="color:#ff9900"/> Unknown';
 		}
 
 		// CPU Temp
 		if($FTL)
 		{
 			if ($celsius >= -273.15) {
-				echo "<a id=\"temperature\"><i class=\"fa fa-fire\" style=\"color:";
+				DominikPITemp = '<i class="fa fa-fire" style="color:';
 				if ($celsius > $temperaturelimit) {
-					echo "#FF0000";
+					DominikPITemp .= "#FF0000";
 				}
 				else
 				{
-					echo "#3366FF";
+					DominikPITemp .="#3366FF";
 				}
-				echo "\"></i> Temp:&nbsp;";
+				DominikPITemp .= '"/> Temp:&nbsp;';
 				if($temperatureunit === "F")
 				{
-					echo round($fahrenheit,1) . "&nbsp;&deg;F";
+					DominikPITemp .= round($fahrenheit,1) . "&nbsp;&deg;F";
 				}
 				elseif($temperatureunit === "K")
 				{
-					echo round($kelvin,1) . "&nbsp;K";
+					DominikPITemp .= round($kelvin,1) . "&nbsp;K";
 				}
 				else
 				{
-					echo round($celsius,1) . "&nbsp;&deg;C";
+					DominikPITemp .= round($celsius,1) . "&nbsp;&deg;C";
 				}
-				echo "</a>";
 			}
 		}
 		else
 		{
-			echo '<a id=\"temperature\"><i class="fa fa-circle" style="color:#FF0000"></i> FTL offline</a>';
+			DominikPITemp = '<i class="fa fa-circle" style="color:#FF0000"/> FTL offline';
 		}
-		?>
-		<br/>
-		<?php
-		echo "<a title=\"Detected $nproc cores\"><i class=\"fa fa-circle\" style=\"color:";
-			if ($loaddata[0] > $nproc) {
-				echo "#FF0000";
-			}
-			else
-			{
-				echo "#7FFF00";
-			}
-			echo "\"></i> Load:&nbsp;&nbsp;" . $loaddata[0] . "&nbsp;&nbsp;" . $loaddata[1] . "&nbsp;&nbsp;". $loaddata[2] . "</a>";
-		?>
-		<br/>
-		<?php
-		echo "<a><i class=\"fa fa-circle\" style=\"color:";
+		
+		DominikPIMem = "<i class=\"fa fa-circle\" style=\"color:";
 			if ($memory_usage > 0.75 || $memory_usage < 0.0) {
-				echo "#FF0000";
+				DominikPIMem .= "#FF0000";
 			}
 			else
 			{
-				echo "#7FFF00";
+				DominikPIMem .= "#7FFF00";
 			}
 			if($memory_usage > 0.0)
 			{
-				echo "\"></i> Memory usage:&nbsp;&nbsp;" . sprintf("%.1f",100.0*$memory_usage) . "&thinsp;%</a>";
+				DominikPIMem .= "\"/> Memory usage:&nbsp;&nbsp;" . sprintf("%.1f",100.0*$memory_usage) . "&thinsp;%";
 			}
 			else
 			{
-				echo "\"></i> Memory usage:&nbsp;&nbsp; N/A</a>";
+				DominikPIMem .= "\"/> Memory usage:&nbsp;&nbsp; N/A";
 			}
+			
+			echo $DominikPIStatus;
+			echo $DominikPITemp;
+			echo $DominikPIMem;
 		?>
     </header>
     <!-- Left side column. contains the logo and sidebar -->
